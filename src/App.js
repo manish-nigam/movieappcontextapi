d6d5f29fc3a0ecde4a8a5ad438b01c8d592fd6e5
MANI12121212 <101326938+MANI12121212@users.noreply.github.com>
2023-07-07 19:02:28 +0530

    movieapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useContext } from 'react';
+// import Login from './component/Login';
+
+// import Home from './component/Home'
+import { Routes,Route,Navigate} from 'react-router-dom';
+// import Allmovies from './component/movies/Allmovies';
+// import Comedy from './component/movies/Comedy'
+// import Drama from './component/movies/Drama'
+// import Action from './component/movies/Action'
+import Moviesdetails from './Component/Moviesdetails';
+import { myContext } from './Component/Context';
+// import TopRated from './component/TopRated';
+import Movies from './Component/Movies';
+
+
+
 
 function App() {
+  // const { login } = useContext(myContext);
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Login/> */}
+        
+        <Routes>
+      
+      
+      <Route path='/' element={<Movies/>}/>
+          {/* <Route path='' element={<Navigate to='toprated'/>}/> 
+          <Route path='toprated' element={login ? <TopRated/> : <Login/> }/>
+          <Route path='allmovies' element={login ? <Allmovies/> : <Login /> }/>
+          <Route path='comedy' element={login ? <Comedy/>: <Login />}/>
+          <Route path='action' element={login ? <Action/>: <Login /> }/>
+          <Route path='drama' element={login ?<Drama/>: <Login />}/> */}
+     
+      
+      <Route path="/MoviedetailPage/:id" element={ <Moviesdetails/>}/>
+    </Routes> 
+
+      
     </div>
   );
 }
